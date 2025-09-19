52cfefaae5125b5a23bd3cb93ccd71251ba62e4f
Anith945 <anitha.putturi2004@gmial.com>
2025-09-19 11:59:42 +0530

    first commit

--- src/pages/Crousel.jsx
+++ src/pages/Crousel.jsx
@@ -12,7 +12,7 @@ export default function Carousel() {
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: tue,
     autoplaySpeed: 3000,
     cssEase: "linear",
   };
