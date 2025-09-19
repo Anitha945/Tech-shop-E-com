30c89563e490cf3a077ca2a83fee3baba9f35227
Anith945 <anitha.putturi2004@gmial.com>
2025-09-19 12:03:32 +0530

    first commit

--- src/projectData/CarouselData.js
+++ src/projectData/CarouselData.js
@@ -22,7 +22,7 @@ export const SliderData = [
               connectivity: "Wireless",
               finalPrice: 9999,
               originalPrice: 14999,
-              quantity: 1
+              quantity: 1,
               ratings: 1234,
               rateCount: 5,
               path: "/product-details/",
