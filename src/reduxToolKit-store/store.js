9a3138567ef07b479fd11235f7d7662bec660279
Anith945 <anitha.putturi2004@gmial.com>
2025-09-19 12:05:32 +0530

    first commit

--- src/reduxToolKit-store/store.js
+++ src/reduxToolKit-store/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolit";
 import { cartReducer, cartMiddlewareFn } from "./productSlice";
 
 //configure store
